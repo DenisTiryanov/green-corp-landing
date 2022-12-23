783332f2b8c74b76dc63101c1a04f2002cea799c
DenisTiryanov <raden007@ya.ru>
2022-12-23 19:36:29 +0300

    Вторая версия проекта

---
+++ test/javascript.js
@@ -0,0 +1,10 @@
+1.	let word = "string";
+2.	
+3.	for (let i = 0; i < 5; i++) { 
+4.	  word += "_1" ; 
+5.	  console.log(word);
+6.	  if (word == "string_1_1_1") { 
+7.	    continue;
+8.	  }
+9.	}
+
